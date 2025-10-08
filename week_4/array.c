18f28b17cdd92dd143ff2dc4a9c9dc2add505216
wesleydonald <152233059+wesleydonald@users.noreply.github.com>
2025-10-08 15:35:29 +1100

    Create array.c

---
+++ week_4/array.c
@@ -0,0 +1,28 @@
+// This program adds 1 to any even values in an array
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int main(void) {
+
+    int array[SIZE] = {1, 2, 3, 4, 5};
+
+    int i = 0;
+    while (i < SIZE) {
+        if (array[i] % 2 == 0) {
+            array[i] += 1;
+        }
+        i++;
+    }
+
+    int j = 0;
+    while (j < SIZE) {
+        printf("%d ", array[j]);
+        j++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
